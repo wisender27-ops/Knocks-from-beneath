a50f00d022c19196b2e1bdddc0bf40a77f999f50
Nikita <wisender27@gmail.com>
2026-01-16 04:28:37 +0300

    Обновил освещение, добавил простой материал стекла и доску, импортировал и переименовал сцену дескифа

--- Assets/Scripts/ItemSystem/PlayerInteraction.cs
+++ Assets/Scripts/ItemSystem/PlayerInteraction.cs
@@ -5,6 +5,7 @@ public class PlayerInteraction : MonoBehaviour
     public float distance = 3f;
     public LayerMask layerMask;
     public PlayerInventory inventory; // Ссылка на наш новый инвентарь
+    public Camera camera;
 
     void Update()
     {
@@ -17,7 +18,7 @@ public class PlayerInteraction : MonoBehaviour
 
     void PerformRaycast()
     {
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        Ray ray = camera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
 
         if (Physics.Raycast(ray, out hit, distance, layerMask))
