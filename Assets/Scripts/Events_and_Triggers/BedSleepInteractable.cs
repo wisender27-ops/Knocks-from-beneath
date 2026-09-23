d038f3a6bb44435c96647c8c2d189554e85e69e9
ENDERWIS <wisender27@gmail.com>
2026-09-23 13:59:26 +0300

    fix: show bed interaction only during sleep quests

--- Assets/Scripts/Events_and_Triggers/BedSleepInteractable.cs
+++ Assets/Scripts/Events_and_Triggers/BedSleepInteractable.cs
@@ -5,8 +5,19 @@ namespace KnocksFromBeneath
 
 public class BedSleepInteractable : MonoBehaviour
 {
+    public bool CanInteract
+    {
+        get
+        {
+            QuestManager quests = QuestManager.Instance;
+            return quests != null &&
+                   (quests.IsQuestActive("go-to-bed") || quests.IsQuestActive("evening-walk"));
+        }
+    }
+
     public void Interact()
     {
+        if (!CanInteract) return;
         GameEvents.OnBedTriggerReached?.Invoke();
     }
 }
